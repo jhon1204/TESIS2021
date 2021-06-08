73e33bb986513177cda2139178d9300cfb09bf3d
Scriptkiddie <a20161589@pucp.edu.pe>
2021-06-08 00:00:01 -0500

    update route 2

--- QairaData/api.py
+++ QairaData/api.py
@@ -278,5 +278,5 @@ def updateAQMap():
 
 atexit.register(lambda: cron.shutdown(wait=False))
 if __name__=="__main__":
-    socketio.run(app,host='127.0.0.1', port=5001)
+    socketio.run(app,host='0.0.0.0', port=5001)
 
